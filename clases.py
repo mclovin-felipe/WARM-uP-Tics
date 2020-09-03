2b4211322d69594b29c2039a27dbb6720a690907
Felipe Ignacio Ponce Canales <felipeignacioponcecanales@MacBook-Air-de-Felipe.local>
2020-09-03 03:52:58 -0400

    Final

---
+++ clases.py
@@ -0,0 +1,65 @@
+class Cliente(object):
+    #Constructor y atributos
+    def __init__(self,cantidad_productos,tiempo_antes_de_caja,tiempo_en_caja, id, atendido):
+        self.cantidad_productos=cantidad_productos
+        self.tiempo_antes_de_caja=tiempo_antes_de_caja
+        self.tiempo_en_caja=tiempo_en_caja
+        self.Patras=None
+        self.atendido=atendido
+        self.id = id
+
+
+class Caja(object):
+    def __init__(self,cola, ide):
+        self.cola=cola
+        self.ocupada = False
+        self.pagando = None
+        self.minimo = False
+        self.identificador = ide
+    
+
+class Cola(object):
+    def __init__(self):
+        self.primero=None
+        self.ultimo=None
+
+    def getVacio(self):
+        if(self.primero==None):
+            return True
+    def Largo(self):
+        if self.getVacio()==True:
+            return 0
+        else:
+            contador=0
+            temporal=self.primero
+            validar=True
+            while(validar):
+                contador+=1
+                if temporal.Patras==None:
+                    validar=False
+                else:
+                    temporal=temporal.Patras
+            return contador
+                
+            
+    def Encolar(self,cliente):
+        if (self.getVacio()==True):
+            self.primero=self.ultimo=cliente
+        else:
+            self.ultimo.Patras=cliente
+            self.ultimo=cliente
+        return True
+    
+    def Desencolar(self):
+        if (self.getVacio()==True):
+            return False
+        elif(self.primero==self.ultimo):
+            self.primero=self.ultimo=None
+            return True
+        
+        else:
+            temp=self.primero
+            self.primero=self.primero.Patras
+            temp=None
+            return True
+
